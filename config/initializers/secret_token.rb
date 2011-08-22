4850215b547451bdf2a507575e8b86e2f0a218a8
Andreas <andreaskatsimperis@gmail.com>
2011-08-22 11:39:21 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SapmleApp::Application.config.secret_token = '90e7d406d0366174c22684daa0f90792a9cd1e30366b34c35259b640189a4764dc79562c384f0b74748061e59c9001f3fcc4b51e3bd80108ee50c465b87eed99'
